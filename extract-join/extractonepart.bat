dee212f96874c336665352e683fdc729bc604e71
Boboaca Costel <bobyca2003@yahoo.com>
2022-12-02 14:08:15 +0100

    Update extractonepart.bat

--- extract-join/extractonepart.bat
+++ extract-join/extractonepart.bat
@@ -1,2 +1,5 @@
+rem extract from 05 to 15
+rem ffmpeg -i video.mp4 -c copy -ss 00:00:05 -t 00:00:10 extract.mp4
+rem extract from 05 to 10
 ffmpeg -i video.mp4 -c copy -ss 00:00:05 -to 00:00:10 extract.mp4
 pause
